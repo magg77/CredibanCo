99c6947a07a25b22462a90df5a19bd6aebd915b5
magg77 <escenariopaloma@gmail.com>
2023-09-14 01:44:07 -0500

    start project Prueba Técnica App  Android

---
+++ LICENSE.md
@@ -0,0 +1,7 @@
+
+
+El aviso de derechos de autor anterior y este aviso de permiso se incluirán en todas las copias o partes sustanciales del Software.
+
+Este proyecto cuenta con licencia conforme a los términos de la licencia propietaria a Juice y no se permite su distribucion, edicion y comercializacion sin concentimiento de su propietario.
+
+EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O IMPLÍCITA, INCLUYENDO PERO NO LIMITADO A LAS GARANTÍAS DE COMERCIABILIDAD, IDONEIDAD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS AUTORES O TITULARES DE DERECHOS DE AUTOR SERÁN RESPONSABLES DE NINGÚN RECLAMO, DAÑO U OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN CONTRACTUAL, AGRAVIO O DE OTRA MANERA, QUE SURJA DE, FUERA DE O EN RELACIÓN CON EL SOFTWARE O EL USO U OTRAS NEGOCIOS EN EL SOFTWARE.
---
+++ app/src/main/java/com/prueba/credibanco/ui/transaction/TransactionFragment.kt
@@ -0,0 +1,42 @@
+package com.prueba.credibanco.ui.transaction
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.prueba.credibanco.databinding.FragmentTransactionBinding
+
+class TransactionFragment : Fragment() {
+
+    private var _binding: FragmentTransactionBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val transactionViewModel =
+            ViewModelProvider(this).get(TransactionViewModel::class.java)
+
+        _binding = FragmentTransactionBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        transactionViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
